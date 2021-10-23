d40ec602f791f76034d788bec92a7cf46db3230a
Cameron Dubas <camerondubas@gmail.com>
2021-10-22 17:20:21 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  createDeployPlugin: function(options) {
+    return {
+      name: options.name,
+
+      runAfter: ['s3-index'],
+
+      willActivate(context) {
+        const inputRevision = context.commandOptions.revision;
+        const targetLatestRevision = inputRevision && inputRevision.toLowerCase() === 'latest';
+
+        if (targetLatestRevision) {
+          const latestRevision = context.initialRevisions[0].revision
+          context.commandOptions.revision = latestRevision;
+        }
+      },
+    };
+  }
 };
